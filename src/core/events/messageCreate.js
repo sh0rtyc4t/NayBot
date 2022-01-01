d4f8e3848145d79542f47254b2594228b5675574
Shortcat37089 <unknown37089@gmail.com>
2022-01-01 14:50:51 -0300

    webhook logs extension midia

--- src/core/events/messageCreate.js
+++ src/core/events/messageCreate.js
@@ -8,10 +8,6 @@ module.exports = async function (message) {
     const content = message.content.split(" ");
     const t = i18.getFixedT(message.member?.guild?.preferredLocale || "en-US");
 
-    if (message.content.match(/^\?\?./)) {
-        return nay.createMessage(message.channel.id, { embeds: [new ctx.BaseEmbed(t("miscellany:alert-slash"), "Changes...")]});
-    }
-
     if (content[0] === `${prefix}eval` && ctx.config.owners.includes(message.author.id)) {
         let text = content.slice(1).join(" ")
             .split(/\r?\n/);
@@ -21,19 +17,14 @@ module.exports = async function (message) {
 
         try {
             const evalued = util.inspect(await eval(`(async()=>{${text}\n})()`), { depth: 1 }).slice(0, 3960);
-            return nay.createMessage(message.channel.id, {
-                embeds: [
-                    {
-                        title: "Eval",
-                        color: ctx.resolveColor("FF0000"),
-                        description: `\`\`\`js\n${evalued}\`\`\``,
-                        timestamp: new Date()
-                    }
-                ]
-            });
+            return message.channel.createMessage({ embeds: [new ctx.BaseEmbed(`\`\`\`js\n${evalued}\`\`\``, "Eval")] });
         } catch (e) {
-            return nay.createMessage(message.channel.id, `Houve um erro na execução do eval:\n\`${e}\``);
+            return message.channel.createMessage(`Houve um erro na execução do eval:\n\`${e}\``);
         }
+    }
+
+    if (message.content.match(/^\?\?./)) {
+        return message.channel.createMessage({ embeds: [new ctx.BaseEmbed(t("miscellany:alert-slash"), "Changes...")]});
     }
 
     if (!message.guildID) {
@@ -41,13 +32,55 @@ module.exports = async function (message) {
             color: ctx.resolveColor("FF0000"),
             title: message.author.tag,
             thumbnail: { url: message.author.dynamicAvatarURL(null, 512)},
-            description: `\`\`\`diff\n- ${message.content}\`\`\``,
+            description: `\`\`\`diff\n- ${message.content || "Sem conteudo"}\`\`\``,
             footer: {
                 icon_url: nay.user.avatarURL,
                 text: `Menssagem em DM de ${nay.user.tag}`
             }
         };
 
+        if (message.attachments.length === 1) embed.image = { url: message.attachments[0].proxy_url };
         hooks.dmLog({ embeds: [embed] });
+
+        if (message.attachments.length > 1 || message.attachments[0].content_type.startsWith("video")) {
+            const embedMidia = new ctx.BaseEmbed(null, "Arquivos da menssagem acima:");
+            const fields = [];
+            const videos = [];
+            const images = [];
+            const files = [];
+
+            for (const attach of message.attachments) {
+                if (attach.content_type.startsWith("video")) {
+                    videos.push(`[${attach.filename}](${attach.proxy_url})`);
+                    continue;
+                }
+
+                if (attach.content_type.startsWith("image")) {
+                    images.push(`[${attach.filename}](${attach.proxy_url})`);
+                    continue;
+                }
+
+                files.push(`[${attach.filename}](${attach.proxy_url})`);
+            }
+
+
+            if (videos.length) fields.push({
+                name: "Videos",
+                value: videos.join("\n")
+            });
+
+            if (images.length) fields.push({
+                name: "Imagens",
+                value: images.join("\n")
+            });
+
+            if (files.length) fields.push({
+                name: "Arquivos",
+                value: files.join("\n")
+            });
+
+            embedMidia.fields = fields;
+            hooks.dmLog({ embeds: [embedMidia] });
+        }
     }
 };
